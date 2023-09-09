18ef55342e1dd82fa918426ca2e410df642ce3c4
fotroxic <fotroxic@gmail.com>
2023-09-09 09:20:58 +0200

    target fix

--- pages/news/entrepreneurship/[slug].js
+++ pages/news/entrepreneurship/[slug].js
@@ -33,9 +33,9 @@ export default function Blog({ frontmatter, markdown}) {
       </ReactMarkdown>
 
       <div className='social__medias'>
-        <a target="_blank" href={`/${frontmatter.facebook}`}><img className='social__media__logos' src='https://cdn-icons-png.flaticon.com/512/124/124010.png'></img></a>
-        <a target="_blank" href={`/${frontmatter.linkedin}`}><img className='social__media__logos' src='https://pbs.twimg.com/profile_images/1661161645857710081/6WtDIesg_400x400.png'></img></a>
-        <a target="_blank" href={`/${frontmatter.twitter}`}><img className='social__media__logos' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/292px-Logo_of_Twitter.svg.png'></img></a>
+        <a target="blank" rel='noreferrer' href={`/${frontmatter.facebook}`}><img className='social__media__logos' src='https://cdn-icons-png.flaticon.com/512/124/124010.png'></img></a>
+        <a target="blank" rel='noreferrer' href={`/${frontmatter.linkedin}`}><img className='social__media__logos' src='https://pbs.twimg.com/profile_images/1661161645857710081/6WtDIesg_400x400.png'></img></a>
+        <a target="blank" rel='noreferrer' href={`/${frontmatter.twitter}`}><img className='social__media__logos' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/292px-Logo_of_Twitter.svg.png'></img></a>
 
       </div>
 
